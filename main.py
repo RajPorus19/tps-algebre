05be33bc22cd071d86ff81729bcb7209d1972835
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2022-10-10 16:43:40 +0200

    save

--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 
 import numpy
 
-def vander(n):
+def vander(n:int):
     liste = list(range(n+1))
     res = numpy.vander(liste,increasing=True)
     return res
 
-def main():
-    print(vander(5))
+def mat_colonne(liste:list):
+    return numpy.array(liste)
+
+def main(n:int):
+    mat_vander = vander(n)
+    #print('La matrice vander associé  à ',n,mat_vander)
+    A=np.linalg.solve(mat_vander, Y)
 
 
 main()
