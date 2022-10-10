27f0219268df0044e76e5e92cd5454bc9de7e41f
Antonin <antonin.juquel@outlook.fr>
2022-10-10 18:08:34 +0200

    NTM

--- tp1.py
+++ tp1.py
@@ -1,28 +1,41 @@
 #!/usr/bin/env python3
 
 import numpy
+import matplotlib.pyplot as pyplot
 
-# inputs
+# Entrée
+
 a = 0
 b = 2 * numpy.pi
-n = 5
-def f(x): return numpy.sin(x)
+n = 250
+# def f(x): return numpy.sin(x)
+def f(x): return [i**2 for i in x]
 
 
-# interval
+# Subdivision de [a,b] en n+1 points équidistants
 X = numpy.linspace(a, b, n)
 
 # V.A = Y
 V = numpy.vander(X, increasing=True)
+
+# Matrice verticale Yi = f(Xi)
 Y = f(X)
+
+# Résolution du système
 A = numpy.linalg.solve(V, Y)
 
+# Évaluation de la fonction
 
-def pol(x):
+
+def P(x):
     s = 0
     for k in range(n):
         s += A[k] * x**k
     return s
 
 
-print(pol(6))
+# Affichage
+pyplot.plot(X, Y)
+pyplot.plot(X, [P(x) for x in X])
+
+pyplot.show()
