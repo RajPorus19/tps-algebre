48b82f8b6fe07faefddb4b0a7c6ed94262afa972
Somindras <57025128+AntoninJuquel@users.noreply.github.com>
2022-11-17 10:15:47 +0100

    Complete code

--- TP5/JUQUEL_HIRUTHAYARAJ_TP5_CODE.py
+++ TP5/JUQUEL_HIRUTHAYARAJ_TP5_CODE.py
@@ -25,7 +25,7 @@ def phi4(x):
 # Allure des courbes
 
 
-x = np.linspace(-1, 2, 100)
+x = np.linspace(0, 1, 500)
 plt.plot(x, [phi1(i) for i in x], label='phi1')
 plt.plot(x, [phi2(i) for i in x], label='phi2')
 plt.plot(x, [phi3(i) for i in x], label='phi3')
@@ -33,25 +33,27 @@ plt.plot(x, [phi4(i) for i in x], label='phi4')
 plt.legend()
 plt.show()
 
-X = [1, 5]
-Y = [6, 2]
-V = [3/2, -3]
+X = [1, 5, 7, 8, 10]
+Y = [6, 2, -1, 1, 2]
+V = [3/2, -3, 0, 4, 1]
 
 # Fonction qui calcule le polynôme d'Hermite P(x)
 
 
 def foncHermite(X, Y, V, x):
     n = len(X)
-    P = Y[0] * phi1((x-X[0])/(X[1]-X[0]))
-    + Y[1] * phi2((x-X[0])/(X[1]-X[0]))
-    + (X[1] - X[0]) * (V[0] * phi3((x-X[0])/(X[1]-X[0])) +
-                       V[1] * phi4((x-X[0])/(X[1]-X[0])))
+    P = 0
+    for k in range(n-1):
+        d = X[k+1]-X[k]
+        t = (x-X[k])/d
+        P += Y[k] * phi1(t) + Y[k+1]*phi2(t) + d*(V[k]*phi3(t)+V[k+1]*phi4(t))
     return P
-
 
 # Allure du polynôme d'Hermite
 
 
-x = np.linspace(0, 6, 100)
+x = np.linspace(0, 10, 500)
 plt.plot(x, [foncHermite(X, Y, V, i) for i in x])
+for k in range(len(X)):
+    plt.plot((X[k]-1, X[k]+1), (Y[k]-V[k], Y[k]+V[k]))
 plt.show()
